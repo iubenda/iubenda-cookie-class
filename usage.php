e35b70efdfd768556721c0c514541a354ec574c1
Sirvasile <vasilealexandrupeste@gmail.com>
2015-09-30 18:43:21 +0200

    Standalone script updated
    Standalone script rewritten with a new method

--- usage.php
+++ usage.php
@@ -1,26 +1,42 @@
 <?php
-	include_once 'simple_html_dom.php';
-	include_once 'iubenda.class.php';
-	
-	// Check if the user has already given consent
-	if(!Page::consent_given()){
 
-		$url = 'http://www.facciamoilpresepe.it';
-		
-		// Here you should pass the content of the page, this is just an example
-		// using file_get_contents(url) to have a real web page
-		$content = file_get_contents(A);
+// the "$html" parameter must contain the content of the web page with the iubenda JavaScript banner/policy included
+ 
+function iubenda_system($html)
+{
+    if(empty($html)) return;
+       
+    /*
+     * Separator
+    */
+ 
+    if(!function_exists("file_get_html")) {
+        require_once("simple_html_dom.php");
+    }
+ 
+    require_once("iubenda.class.php");
+ 
+    /*
+     * Separator
+    */
+ 
+    if(!Page::consent_given() && !Page::bot_detected()) {
+        $page = new Page($html);
+        $page->parse();
+        $html = $page->get_converted_page();
+    }
+ 
+    /* Finished */
+ 
+   	return $html;
+}
 
-		// Istantiate new Page with content
-		$page = new Page($content);
-		
-		// Parse and convert content
-		$page->parse();
-		
-		// Print the new page
-		echo $page->get_converted_page();
-	}else{
-		echo 'Consent already given.. print the page without instantiate and parse';
-	}
-	
+/*
+ *
+ * Example:
+ *
+ * echo iubenda_system("<html> ...content... </html>");
+ *
+*/
+
 ?>
