6d308b6b8ad0d0cddbcde10da3150111197a5cf0
Simon Schmid <zimonschmid@gmail.com>
2015-11-24 18:37:45 +0100

    Adding copyright/license info as requested by J!
    https://docs.joomla.org/JED_Entries_License_Checklist

--- test.php
+++ test.php
@@ -1,4 +1,22 @@
 <?php
+/**
+ * test.php
+ * @author: Copyright 2015 iubenda
+ * @license GNU/GPL
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 ini_set('max_execution_time', 300);
 ?>
 
@@ -25,12 +43,12 @@ ul li { list-style-type: none; }
 				<input type="submit" class="btn" value="Analyze">
 			</form>
 		</div>
-	
+
 <?php
-	
+
 	$url = $_POST['url'];
 	if($url || $_GET['url']){
-		
+
 	function print_stuff($array){
 		if(count($array)){
 			echo "<ul>";
@@ -42,17 +60,17 @@ ul li { list-style-type: none; }
 			echo "<p>Nothing</p>";
 		}
 	}
-	
+
 	echo '<div class="col-md-12" style="padding-bottom:150px;"><h2>RESULTS</H2>';
 	include_once 'iubenda.class.php';
 	include_once 'simple_html_dom.php';
-	
+
 	if($_GET['url']){
 		$content = file_get_contents('A');
 	}else{
 		$content = file_get_contents($url);
 	}
-	
+
 	$page = new Page($content);
 	$page->parse();
 
@@ -62,35 +80,35 @@ ul li { list-style-type: none; }
 	$iframe_converted = count($page->iframe_converted);
 	$script_inline_converted = count($page->scripts_inline_converted);
 	$script_converted = count($page->scripts_converted);
-	
-	echo "<p>Iubenda comments detected: $iub_comments_detected<br>Iubenda automatic stuff<br>Iframe detected: $iframe_detected<br>Iframe autoconverted: $iframe_converted<br>Scripts detected: $scripts_detected<br>Inline scripts autoconverted: $script_inline_converted<br>Scripts autoconverted: $script_converted</p>";
-	
 
-	
-	
-	
+	echo "<p>Iubenda comments detected: $iub_comments_detected<br>Iubenda automatic stuff<br>Iframe detected: $iframe_detected<br>Iframe autoconverted: $iframe_converted<br>Scripts detected: $scripts_detected<br>Inline scripts autoconverted: $script_inline_converted<br>Scripts autoconverted: $script_converted</p>";
+
+
+
+
+
 	echo "<H3>DETAILS</H3>";
-	
+
 	echo "<H4>iubenda comments stuff</h4>";
 	print_stuff($page->iub_comments_detected);
-		
+
 	echo "<H4>Script detected</h4>";
 	print_stuff($page->scripts_detected);
 	echo "<H4>Script converted</h4>";
 	print_stuff($page->scripts_converted);
 	echo "<H4>Script inline converted</h4>";
 	print_stuff($page->scripts_inline_converted);
-	
+
 	echo "<H4>Iframe detected</h4>";
 	print_stuff($page->iframe_detected);
 	echo "<H4>Iframe converted</h4>";
 	print_stuff($page->iframe_converted);
-	
-	
-	
+
+
+
 	echo "</div>";
-	
-	
+
+
 	}
 ?>
 </div>
--- usage.php
+++ usage.php
@@ -1,33 +1,50 @@
 <?php
+/**
+ * usage.php
+ * @author: Copyright 2015 iubenda
+ * @license GNU/GPL
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 
 // the "$html" parameter must contain the content of the web page with the iubenda JavaScript banner/policy included
- 
+
 function iubenda_system($html)
 {
     if(empty($html)) return;
-       
+
     /*
      * Separator
     */
- 
+
     if(!function_exists("file_get_html")) {
         require_once("simple_html_dom.php");
     }
- 
+
     require_once("iubenda.class.php");
- 
+
     /*
      * Separator
     */
- 
+
     if(!Page::consent_given() && !Page::bot_detected()) {
         $page = new Page($html);
         $page->parse();
         $html = $page->get_converted_page();
     }
- 
+
     /* Finished */
- 
+
    	return $html;
 }
 
